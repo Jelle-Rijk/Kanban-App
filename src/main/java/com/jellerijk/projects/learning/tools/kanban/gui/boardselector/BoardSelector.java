f83e7d2efeff1dc994921d855bc2f8c80c2740ec
Jelle-Rijk <jellerijk@gmail.com>
2026-02-04 17:05:37 +0000

    Added a Button that opens a BoardCreator.

--- src/main/java/com/jellerijk/projects/learning/tools/kanban/gui/boardselector/BoardSelector.java
+++ src/main/java/com/jellerijk/projects/learning/tools/kanban/gui/boardselector/BoardSelector.java
@@ -3,19 +3,26 @@ package com.jellerijk.projects.learning.tools.kanban.gui.boardselector;
 import java.util.List;
 
 import com.jellerijk.projects.learning.tools.kanban.domain.board.BoardController;
+import com.jellerijk.projects.learning.tools.kanban.gui.boardCreator.BoardCreator;
 import com.jellerijk.projects.learning.tools.kanban.logging.Logger;
 import com.jellerijk.projects.learning.tools.kanban.persistence.dto.BoardDTO;
 import com.jellerijk.projects.learning.tools.kanban.utils.PublishedMessageType;
 import com.jellerijk.projects.learning.tools.kanban.utils.Subscriber;
 
+import javafx.event.ActionEvent;
 import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 
 public class BoardSelector extends AnchorPane implements Subscriber {
 	private final BoardController bc;
+
 	private VBox boardCards;
+	private Button btnAdd;
 
 	public BoardSelector() {
 		bc = BoardController.getInstance();
@@ -34,10 +41,16 @@ public class BoardSelector extends AnchorPane implements Subscriber {
 		scrollPane.setFitToWidth(true);
 		scrollPane.setMaxHeight(200);
 		AnchorPane.setTopAnchor(scrollPane, (double) 10);
-		AnchorPane.setBottomAnchor(scrollPane, (double) 10);
+		AnchorPane.setBottomAnchor(scrollPane, (double) 40);
 		AnchorPane.setLeftAnchor(scrollPane, (double) 10);
 		AnchorPane.setRightAnchor(scrollPane, (double) 10);
 		this.getChildren().add(scrollPane);
+
+		Button btnAdd = new Button("Add board");
+		btnAdd.setOnAction(e -> openBoardCreator(e));
+		this.getChildren().add(btnAdd);
+		AnchorPane.setBottomAnchor(btnAdd, (double) 5);
+		AnchorPane.setLeftAnchor(btnAdd, (double) 5);
 
 		this.setPadding(new Insets(16));
 		update();
@@ -45,10 +58,19 @@ public class BoardSelector extends AnchorPane implements Subscriber {
 
 	@Override
 	public void update(PublishedMessageType messageType) {
-		Logger.log("Boardselector updated");
 		boardCards.getChildren().clear();
 		List<BoardDTO> boards = bc.getBoards();
 		boards.forEach(board -> boardCards.getChildren().add(new BoardCard(bc, board.id())));
 	}
+
+	private void openBoardCreator(ActionEvent event) {
+		Logger.log("Opening boardCreator");
+		BoardCreator root = new BoardCreator();
+		Scene scene = new Scene(root);
+		Stage stage = new Stage();
+		stage.setScene(scene);
+		stage.setTitle("Create New Board");
+		stage.show();
+	};
 
 }
