dc130d5ec3460825b163b774bcd17529b8da680f
Jelle-Rijk <jellerijk@gmail.com>
2026-02-05 10:23:41 +0000

    Initial board layout

--- src/main/java/com/jellerijk/projects/learning/tools/kanban/gui/board/BoardView.java
+++ src/main/java/com/jellerijk/projects/learning/tools/kanban/gui/board/BoardView.java
@@ -1,8 +1,11 @@
 package com.jellerijk.projects.learning.tools.kanban.gui.board;
 
 import com.jellerijk.projects.learning.tools.kanban.domain.board.BoardController;
+import com.jellerijk.projects.learning.tools.kanban.gui.stage.StageView;
 import com.jellerijk.projects.learning.tools.kanban.persistence.dto.BoardDTO;
+import com.jellerijk.projects.learning.tools.kanban.persistence.dto.StageDTO;
 
+import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
@@ -19,9 +22,14 @@ public class BoardView extends ScrollPane {
 	private void buildGUI() {
 		BorderPane bp = new BorderPane();
 		setContent(bp);
-		
+
+		bp.setTop(new Label(board.name()));
 		HBox stages = new HBox();
 		bp.setCenter(stages);
-		
+		StageDTO testDTO = new StageDTO(1, "Testlist", -1, "List to test with.", 3);
+		StageDTO testDTO2 = new StageDTO(2, "Stage 2", -1, "List to test with.", 3);
+		stages.getChildren().add(new StageView(testDTO));
+		stages.getChildren().add(new StageView(testDTO2));
+
 	}
 }
--- src/main/java/com/jellerijk/projects/learning/tools/kanban/gui/stage/StageView.java
+++ src/main/java/com/jellerijk/projects/learning/tools/kanban/gui/stage/StageView.java
@@ -1,5 +1,51 @@
 package com.jellerijk.projects.learning.tools.kanban.gui.stage;
 
-public class StageView {
+import com.jellerijk.projects.learning.tools.kanban.persistence.dto.StageDTO;
+
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+
+public class StageView extends AnchorPane {
+	private final static double DEFAULT_WIDTH = 200;
+	private final static double DEFAULT_MAX_HEIGHT = 300;
+	private String title;
+	private VBox taskList;
+	private double width;
+	private double maxHeight;
+
+	public StageView(StageDTO stage, double width, double maxHeight) {
+		this.width = width;
+		this.maxHeight = maxHeight;
+		title = stage.title();
+		buildGUI();
+	}
+
+	public StageView(StageDTO stage) {
+		this(stage, DEFAULT_WIDTH, DEFAULT_MAX_HEIGHT);
+	}
+
+	private void buildGUI() {
+		Label lblTitle = new Label(title);
+		AnchorPane.setTopAnchor(lblTitle, 5.0);
+		AnchorPane.setLeftAnchor(lblTitle, 5.0);
+
+		ScrollPane scrollPane = new ScrollPane();
+		AnchorPane.setTopAnchor(scrollPane, 30.0);
+		AnchorPane.setBottomAnchor(scrollPane, 50.0);
+		scrollPane.setContent(taskList);
+		scrollPane.setMinWidth(width);
+		scrollPane.setFitToHeight(true);
+		scrollPane.setPrefHeight(Double.MAX_VALUE);
+		scrollPane.setMaxHeight(maxHeight);
+
+		Button btnAdd = new Button("Add task");
+		AnchorPane.setBottomAnchor(btnAdd, 5.0);
+
+		getChildren().addAll(lblTitle, scrollPane, btnAdd);
+
+	}
 
 }
