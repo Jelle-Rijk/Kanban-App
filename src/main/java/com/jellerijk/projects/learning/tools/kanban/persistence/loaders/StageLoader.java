7ff46c1997f30579ae2d7276ac59b6a1b1f156d1
Jelle-Rijk <jellerijk@gmail.com>
2026-02-06 18:17:34 +0000

    Added StageLoader

---
+++ src/main/java/com/jellerijk/projects/learning/tools/kanban/persistence/loaders/StageLoader.java
@@ -0,0 +1,45 @@
+package com.jellerijk.projects.learning.tools.kanban.persistence.loaders;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Collection;
+
+import com.jellerijk.projects.learning.tools.kanban.logging.Logger;
+import com.jellerijk.projects.learning.tools.kanban.persistence.database.DBController;
+import com.jellerijk.projects.learning.tools.kanban.persistence.dto.StageDTO;
+
+public abstract class StageLoader {
+	public static Collection<StageDTO> loadByBoard(int boardId) throws SQLException {
+		PreparedStatement pstmt = DBController.getInstance().prepareStatement("SELECT * FROM Stage WHERE Board = ?");
+		pstmt.setInt(1, boardId);
+		ResultSet results = pstmt.executeQuery();
+		return convertResultSet(results);
+	}
+
+	public static Collection<StageDTO> loadAll() {
+		ResultSet results = DBController.getInstance().query("SELECT * FROM Stage");
+		return convertResultSet(results);
+	}
+
+	private static Collection<StageDTO> convertResultSet(ResultSet results) {
+		Collection<StageDTO> stages = new ArrayList<StageDTO>();
+		try {
+			while (results.next()) {
+				int board = results.getInt("BoardId");
+				int number = results.getInt("Number");
+				String title = results.getString("Title");
+				String description = results.getString("Description");
+				int limit = results.getInt("Limit");
+
+				StageDTO stage = StageDTO.create(number, title, board, description, limit);
+				stages.add(stage);
+			}
+		} catch (SQLException e) {
+			Logger.logError("Something went wrong while trying to convert ResultSet to Boards.");
+			e.printStackTrace();
+		}
+		return stages;
+	}
+}
