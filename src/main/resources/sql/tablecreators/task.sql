cce2dc5a810624bb126c9c2a792f2ffc7f5472cc
Jelle-Rijk <jellerijk@gmail.com>
2026-02-06 16:18:13 +0000

    Added completed field.

--- src/main/resources/sql/tablecreators/task.sql
+++ src/main/resources/sql/tablecreators/task.sql
@@ -3,6 +3,7 @@ CREATE TABLE IF NOT EXISTS "Task" (
 	"Description"	TEXT NOT NULL,
 	"Stage"	INTEGER NOT NULL,
 	"BoardId"	INTEGER NOT NULL,
+	"Completed"	INTEGER NOT NULL CHECK(Completed IN (0, 1)),
 	FOREIGN KEY("Stage") REFERENCES "Stage"("StageNumber"),
 	PRIMARY KEY("TaskId" AUTOINCREMENT),
 	FOREIGN KEY("BoardId") REFERENCES "Board"("BoardId")
