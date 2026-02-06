111d5d3121a07d1bac435bba326688b54c6765b9
Jelle-Rijk <jellerijk@gmail.com>
2026-02-06 15:19:12 +0000

    Added completed field.

--- src/main/java/com/jellerijk/projects/learning/tools/kanban/persistence/dto/TaskDTO.java
+++ src/main/java/com/jellerijk/projects/learning/tools/kanban/persistence/dto/TaskDTO.java
@@ -2,9 +2,10 @@ package com.jellerijk.projects.learning.tools.kanban.persistence.dto;
 
 import com.jellerijk.projects.learning.tools.kanban.domain.task.Task;
 
-public record TaskDTO(int id, String description, int boardId, int stageNumber) {
+public record TaskDTO(int id, String description, int boardId, int stageNumber, boolean completed) {
 	public static TaskDTO convert(Task task) {
-		return new TaskDTO(task.getId(), task.getDescription(), task.getBoardId(), task.getStageNumber())	;
+		return new TaskDTO(task.getId(), task.getDescription(), task.getBoardId(), task.getStageNumber(),
+				task.isCompleted());
 	}
 
 }
