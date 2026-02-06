06e7a4f8fc04bae0fa87c00149841a95891f9dc6
Jelle-Rijk <jellerijk@gmail.com>
2026-02-06 16:56:24 +0000

    Added Description field.

--- src/main/resources/sql/tablecreators/stage.sql
+++ src/main/resources/sql/tablecreators/stage.sql
@@ -2,6 +2,7 @@ CREATE TABLE IF NOT EXISTS "Stage" (
 	"Number" INTEGER,
 	"BoardId" INTEGER,
 	"Title" TEXT NOT NULL,
+	"Description" TEXT,
 	"Limit" INTEGER,
 	PRIMARY KEY("Number","BoardId"),
 	FOREIGN KEY("BoardId") REFERENCES "Board"("BoardId")
