3a8affddcbef567bade622ef912e0639b25f4d89
Jelle-Rijk <jellerijk@gmail.com>
2026-02-14 21:30:31 +0000

    Removed: BoardLoader, BoardInserter, Added: BoardMapper

---
+++ src/main/java/com/jellerijk/projects/learning/tools/kanban/persistence/mappers/BoardMapper.java
@@ -0,0 +1,76 @@
+package com.jellerijk.projects.learning.tools.kanban.persistence.mappers;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Collection;
+
+import com.jellerijk.projects.learning.tools.kanban.domain.board.Board;
+import com.jellerijk.projects.learning.tools.kanban.domain.board.BoardImpl;
+import com.jellerijk.projects.learning.tools.kanban.exceptions.DatabaseInsertException;
+import com.jellerijk.projects.learning.tools.kanban.logging.Logger;
+import com.jellerijk.projects.learning.tools.kanban.persistence.database.DBController;
+
+public class BoardMapper implements Mapper<Board> {
+	private Connection conn;
+
+	private final static String TABLE = "Board";
+	private final static String COL_ID = "BoardId";
+	private final static String COL_NAME = "Name";
+	private final static String COL_DESCRIPTION = "Description";
+
+	private final static String INSERT_BOARD = String.format("INSERT INTO %s (%s, %s) VALUES (?,?)", TABLE, COL_NAME,
+			COL_DESCRIPTION);
+	private final static String QUERY_ALL = String.format("SELECT * FROM %s", TABLE);
+
+	public BoardMapper() {
+		conn = DBController.getInstance().getConnection();
+	}
+
+//	CREATE
+	@Override
+	public void insert(Board board) throws DatabaseInsertException {
+		try (PreparedStatement query = conn.prepareStatement(INSERT_BOARD)) {
+			query.setString(1, board.getName());
+			query.setString(2, board.getDescription());
+			query.execute();
+			Logger.log("Inserted a Board into the database.");
+		} catch (SQLException e) {
+			Logger.logError("Something went wrong while inserting a Board");
+			throw new DatabaseInsertException();
+		}
+	}
+
+//	READ
+	@Override
+	public Collection<Board> getAll() {
+		Collection<Board> boards = new ArrayList<Board>();
+		try (PreparedStatement query = conn.prepareStatement(QUERY_ALL)) {
+			ResultSet results = query.executeQuery();
+			boards = mapResults(results);
+			Logger.log(String.format("Loaded %d Board records.", boards.size()));
+		} catch (SQLException e) {
+			Logger.logError("Something went wrong while retrieving all Boards from database.");
+			Logger.logError(e);
+			e.printStackTrace();
+		}
+		return boards;
+	}
+
+	// HELPER METHODS
+	private Collection<Board> mapResults(ResultSet results) throws SQLException {
+		Collection<Board> boards = new ArrayList<Board>();
+		while (results.next()) {
+			int id = results.getInt(COL_ID);
+			String name = results.getString(COL_NAME);
+			String description = results.getString(COL_DESCRIPTION);
+
+			Board board = new BoardImpl(id, name, description);
+			boards.add(board);
+		}
+		return boards;
+	}
+
+}
