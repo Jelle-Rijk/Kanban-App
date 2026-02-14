03899eaff347816e07e1b0a4a7e72f1e0534c1aa
Jelle-Rijk <jellerijk@gmail.com>
2026-02-14 23:00:34 +0000

    Changed order of functions.

--- src/main/java/com/jellerijk/projects/learning/tools/kanban/persistence/mappers/Mapper.java
+++ src/main/java/com/jellerijk/projects/learning/tools/kanban/persistence/mappers/Mapper.java
@@ -4,15 +4,6 @@ import java.util.Collection;
 
 public interface Mapper<T> {
 	/**
-	 * Returns a Collection of all entries associated with this datatype in the
-	 * database.
-	 * 
-	 * @return Collection of all entries in the database associated with this
-	 *         datatype.
-	 */
-	public Collection<T> getAll();
-
-	/**
 	 * Maps object to a corresponding Database entry.
 	 * 
 	 * @param object
@@ -20,6 +11,15 @@ public interface Mapper<T> {
 	 *         not auto-incremented.
 	 */
 	public int insert(T object);
+
+	/**
+	 * Returns a Collection of all entries associated with this datatype in the
+	 * database.
+	 * 
+	 * @return Collection of all entries in the database associated with this
+	 *         datatype.
+	 */
+	public Collection<T> getAll();
 
 	/**
 	 * Removes the entry associated to this object from the database.
