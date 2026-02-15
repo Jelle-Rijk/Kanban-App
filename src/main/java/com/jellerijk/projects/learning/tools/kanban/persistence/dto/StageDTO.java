6bd6cbef4c48fdd982cb33d5980003bb2a8cc825
Jelle-Rijk <jellerijk@gmail.com>
2026-02-15 20:17:44 +0000

    Added Id field.

--- src/main/java/com/jellerijk/projects/learning/tools/kanban/persistence/dto/StageDTO.java
+++ src/main/java/com/jellerijk/projects/learning/tools/kanban/persistence/dto/StageDTO.java
@@ -2,13 +2,13 @@ package com.jellerijk.projects.learning.tools.kanban.persistence.dto;
 
 import com.jellerijk.projects.learning.tools.kanban.domain.stage.Stage;
 
-public record StageDTO(int number, String title, int boardId, String description, int limit) {
+public record StageDTO(int id, int number, String title, int boardId, String description, int limit) {
 	public static StageDTO convert(Stage stage) {
-		return new StageDTO(stage.getNumber(), stage.getTitle(), stage.getBoardId(), stage.getDescription(),
-				stage.getLimit());
+		return new StageDTO(stage.getId(), stage.getNumber(), stage.getTitle(), stage.getBoardId(),
+				stage.getDescription(), stage.getLimit());
 	};
 
-	public static StageDTO create(int number, String title, int boardId, String description, int limit) {
-		return new StageDTO(number, title, boardId, description, limit);
+	public static StageDTO create(int id, int number, String title, int boardId, String description, int limit) {
+		return new StageDTO(id, number, title, boardId, description, limit);
 	};
 }
