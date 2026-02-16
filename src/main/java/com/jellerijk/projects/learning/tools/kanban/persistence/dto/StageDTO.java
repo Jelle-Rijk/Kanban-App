de9e5318bc06dc1cf6bf998c9875584bf255d2ff
Jelle-Rijk <jellerijk@gmail.com>
2026-02-16 21:30:30 +0000

    Updated to new Stage data.

--- src/main/java/com/jellerijk/projects/learning/tools/kanban/persistence/dto/StageDTO.java
+++ src/main/java/com/jellerijk/projects/learning/tools/kanban/persistence/dto/StageDTO.java
@@ -1,14 +1,13 @@
 package com.jellerijk.projects.learning.tools.kanban.persistence.dto;
 
-import com.jellerijk.projects.learning.tools.kanban.domain.stage.Stage;
+import com.jellerijk.projects.learning.tools.kanban.domain.Stage;
 
-public record StageDTO(int id, int number, String title, int boardId, String description, int limit) {
+public record StageDTO(String id, String name, int limit) {
 	public static StageDTO convert(Stage stage) {
-		return new StageDTO(stage.getId(), stage.getNumber(), stage.getTitle(), stage.getBoardId(),
-				stage.getDescription(), stage.getLimit());
+		return new StageDTO(stage.getId(), stage.getName(), stage.getLimit());
 	};
 
-	public static StageDTO create(int id, int number, String title, int boardId, String description, int limit) {
-		return new StageDTO(id, number, title, boardId, description, limit);
+	public static StageDTO create(String id, String name, int limit) {
+		return new StageDTO(id, name, limit);
 	};
 }
