f52bedf7b7c65c9dde017b006786d9738314b51a
Jelle-Rijk <jellerijk@gmail.com>
2026-02-16 17:52:50 +0000

    ID is now a String.

--- src/main/java/com/jellerijk/projects/learning/tools/kanban/domain/Repository.java
+++ src/main/java/com/jellerijk/projects/learning/tools/kanban/domain/Repository.java
@@ -1,11 +1,11 @@
 package com.jellerijk.projects.learning.tools.kanban.domain;
 
 public interface Repository<T> {
-	public int add(T object);
+	public String add(T object);
 
 	public T getById(String id) throws IllegalArgumentException;
 
-	public int update(T object);
+	public void update(T object);
 
 	public void delete(String id) throws IllegalArgumentException;
 
