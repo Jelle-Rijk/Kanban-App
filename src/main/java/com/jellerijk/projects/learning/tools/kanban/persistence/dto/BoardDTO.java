25aeaea091eee7c5bbd91e667eafae15ef10ee7a
Jelle-Rijk <jellerijk@gmail.com>
2026-02-16 20:43:25 +0000

    Updated Id to be a String.

--- src/main/java/com/jellerijk/projects/learning/tools/kanban/persistence/dto/BoardDTO.java
+++ src/main/java/com/jellerijk/projects/learning/tools/kanban/persistence/dto/BoardDTO.java
@@ -1,13 +1,13 @@
 package com.jellerijk.projects.learning.tools.kanban.persistence.dto;
 
-import com.jellerijk.projects.learning.tools.kanban.domain.board.Board;
+import com.jellerijk.projects.learning.tools.kanban.domain.Board;
 
-public record BoardDTO(int id, String name, String description) {
+public record BoardDTO(String id, String title, String description) {
 	public static BoardDTO convert(Board board) {
-		return new BoardDTO(board.getId(), board.getName(), board.getDescription());
+		return new BoardDTO(board.getId(), board.getTitle(), board.getDescription());
 	};
 
-	public static BoardDTO create(int id, String name, String description) {
-		return new BoardDTO(id, name, description);
+	public static BoardDTO create(String id, String title, String description) {
+		return new BoardDTO(id, title, description);
 	}
 }
